0c095f7ea3d97b901b6b99785df3e8279f70081b
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-27 16:08:14 +0530

    hard coded todo displayed

--- code_with_harry/cwh-todo-list/src/app/MyComponents/todos/todos.component.ts
+++ code_with_harry/cwh-todo-list/src/app/MyComponents/todos/todos.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  todos: Todo[] = [];
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: 'This is title1',
+        desc: 'Description',
+        active: true,
+      },
+      {
+        sno: 2,
+        title: 'This is title2',
+        desc: 'Description',
+        active: true,
+      },
+      {
+        sno: 3,
+        title: 'This is title3',
+        desc: 'Description',
+        active: true,
+      },
+    ];
   }
 
+  ngOnInit(): void {}
+
+  deleteTodo(todo: Todo) {
+    console.log(todo);
+    // this.todos = this.todos.filter((prevTodo) => {
+    //   return prevTodo.sno !== todo.sno;
+    // });
+
+    this.todos.splice(this.todos.indexOf(todo), 1);
+  }
 }
